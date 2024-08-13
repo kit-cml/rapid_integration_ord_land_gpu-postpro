e9cb542b678d640d933804340e33b1145c55fd78
IritaSee <iga.narendra@gmail.com>
2024-08-13 17:15:12 +0900

    add test data

--- bin/codetest.sh
+++ bin/codetest.sh
@@ -1,4 +1,4 @@
 cd ..
 make clean all
 cd bin
-./drug_sim -input_deck input_deck_example.txt -hill_file control_me/control.csv -init_file control_me/init_cell1.csv
+./drug_sim -input_deck input_deck_example.txt -hill_file ./IC50_control.csv -init_file result/init_control/state_only.csv
